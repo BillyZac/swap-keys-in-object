86e7c854d6c87869b99db07dd2c1eaa651d6e7ef
Zachary Smith <william.smith1@wipro.com>
2016-08-19 16:07:40 -0400

    :bug: Typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const swapKeysInObject = (keyMap, object) => Object.keys(keyMap).reduce((newObject, key) => {
         const newKey = keyMap[key]
-        newObject[newKey] = project[key]
+        newObject[newKey] = object[key]
         return newObject
     }, {})
 
